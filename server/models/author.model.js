5d2d5285a03543f96fc23fc65594da5f73bb3870
alexander-ponce <alex@poncealex.com>
2023-05-22 18:05:18 -0700

    Updated with Edit functionality

--- server/models/author.model.js
+++ server/models/author.model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+// const {isName} = require("validators");
+
 const AuthorSchema = new mongoose.Schema({
     name: { type: String,
         required: [true, "Name must be at least 2 characters long"],
         minlength: [2, "Name must be at least 2 characters long"],
-        maxlength: [55, "Name must be at least 55 characters long"]
+        maxlength: [55, "Name must be less than 55 characters long"],
+        unique: [true, "Name must be unique"]
 
     }
 }, { timestamps: true });
